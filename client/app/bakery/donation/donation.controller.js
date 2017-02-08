2156de23a6c9d1629e5f23199ee2868f03876e37
Arthur LANNELUCQ <arthur.lannelucq&outlook.fr>
2017-02-08 17:13:43 +0100

    Interface de don - Ajout d'éléments

--- client/app/bakery/donation/donation.controller.js
+++ client/app/bakery/donation/donation.controller.js
@@ -4,7 +4,27 @@
 
 class DonationComponent {
   constructor() {
+    this.cardNumber = 1;
+    this.listDonations = [];
+    this.item = {};
+  }
 
+  setPreviousCardNumber() {
+    this.cardNumber --;
+  }
+
+  setNextCardNumber() {
+    this.cardNumber ++;
+  }
+
+  addDonationToList() {
+    this.listDonations.push(this.item);
+    this.item = {};
+  }
+
+  debug() {
+    console.log(this.item);
+    console.log(this.listDonations);
   }
 }
 
